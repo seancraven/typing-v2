cdf295bd58d0ca85595c85699ec817e0abf03b97
sean craven <seanpietercraven@gmail.com>
2024-08-27 22:26:09 +0100

    fix: got it my god.

--- js/itext.js
+++ js/itext.js
@@ -1,60 +1,70 @@
+var no_col = "bg-white text-gray-900";
+var right_col = "text-green-200";
+var wrong_col = "text-red-200";
+var next_col = "bg-sky-200 text-gray-900";
 var TextEntry = /** @class */ (function () {
     function TextEntry(html) {
         var _this = this;
         this.html = html;
-        this.start_state = function () {
-            _this.current_char = _this.html.innerHTML[0];
-            var _a = replaceCharWithSpan(_this.html.innerHTML, 0, _this.html.innerHTML[0], "text-grey-300 bg-sky-200"), new_txt = _a[0], new_start = _a[1], new_end = _a[2];
-            _this.html.innerHTML = new_txt;
-            _this.span_end = new_end;
-            _this.span_start = new_start;
-            console.assert(_this.html.innerHTML[_this.span_end] == ">", "Break in start", new_txt[new_end]);
-        };
         /**
          * update_state
          */
         this.update_state = function (ev) {
-            var _a;
             ev.preventDefault();
+            _this.update_forward(ev);
+        };
+        this.update_forward = function (ev) {
             var color;
-            if (ev.key == _this.current_char) {
-                color = "text-green-200";
+            var current_char = _this.html.children[_this.n].innerHTML;
+            if (ev.key == current_char) {
+                color = right_col;
             }
             else {
-                color = "text-red-500";
+                color = wrong_col;
             }
-            console.assert(_this.html.innerHTML[_this.span_end] == ">", "Break in start", _this.html.innerHTML[_this.span_end]);
-            var _b = replaceSpanWithSpan(_this.html.innerHTML, _this.span_start, _this.span_end, ev.key, color), new_txt = _b[0], new_start = _b[1], new_end = _b[2];
-            console.assert(new_txt[new_end] == ">", "Break after replace span with span");
-            _a = replaceCharWithSpan(new_txt, new_end + 1, new_txt[new_end + 1], "text-grey-300 bg-sky-200"), new_txt = _a[0], new_start = _a[1], new_end = _a[2];
-            _this.html.innerHTML = new_txt;
-            _this.span_start = new_start;
-            _this.span_end = new_end;
-            console.assert(_this.html.innerHTML[_this.span_end] == ">", "Break after replace char with span");
+            colorSpan(_this.html, _this.html.children[_this.n], color);
+            _this.n++;
+            if (_this.n == _this.html.children.length) {
+                document.removeEventListener("keydown", _this.update_state);
+                return;
+            }
+            colorSpan(_this.html, _this.html.children[_this.n], next_col);
+        };
+        this.update_backward = function (ev) {
+            if (ev.key != "Backspace") {
+                return;
+            }
+            if (_this.n == 0) {
+                return;
+            }
+            colorSpan(_this.html, _this.html.children[_this.n], no_col);
+            _this.html.children[_this.n];
+            console.log(_this.n);
+            _this.n--;
+            colorSpan(_this.html, _this.html.children[_this.n], next_col);
+        };
+        this.spanify = function () {
+            var new_text = new Array();
+            var char;
+            for (var i = 0; i < _this.html.innerHTML.length; i++) {
+                char = _this.html.innerHTML[i];
+                new_text.push("<span class=".concat(no_col, ">") + char + "</span>");
+            }
+            _this.html.innerHTML = new_text.join("");
         };
         this.n = 0;
-        this.span_start = 0;
-        this.span_end = 0;
         this.html = html;
-        this.seen_start_ends = new Array();
+        this.spanify();
+        colorSpan(this.html, this.html.children[0], next_col);
     }
     return TextEntry;
 }());
-function replaceCharWithSpan(string_, index, char_to_replace, color) {
-    var replacement_string = '<span class = "' + color + '">' + char_to_replace + "</span>";
-    var new_n = index + replacement_string.length - 1;
-    var updated_string = string_.substring(0, index) +
-        replacement_string +
-        string_.substring(index + 1);
-    return [updated_string, index, new_n];
-}
-function replaceSpanWithSpan(string_, index_start, index_end, char_to_replace, color) {
-    var replacement_string = '<span class = "' + color + '">' + char_to_replace + "</span>";
-    var new_n = index_start + replacement_string.length - 1;
-    var updated_string = string_.substring(0, index_start) +
-        replacement_string +
-        string_.substring(index_end - 1);
-    return [updated_string, index_start, new_n];
+function colorSpan(parent, span, color) {
+    var char = span.innerHTML;
+    var new_span = document.createElement("span");
+    new_span.className = color;
+    new_span.innerHTML = char;
+    parent.replaceChild(new_span, span);
 }
 var p = document.getElementById("input_text");
 if (!p) {
@@ -67,5 +77,5 @@ else {
 console.log("Found input text");
 var n = 0;
 var text_entry = new TextEntry(p);
-text_entry.start_state();
 document.addEventListener("keypress", text_entry.update_state);
+document.addEventListener("keydown", text_entry.update_backward);
--- js/itext.ts
+++ js/itext.ts
@@ -1,110 +1,70 @@
+const no_col = "bg-white text-gray-900";
+const right_col = "text-green-200";
+const wrong_col = "text-red-200";
+const next_col = "bg-sky-200 text-gray-900";
 class TextEntry {
   private n: number;
-  private current_char: string;
-  private span_start: number;
-  private span_end: number;
-  private seen_start_ends: [number, number][];
   constructor(public html: HTMLElement) {
     this.n = 0;
-    this.span_start = 0;
-    this.span_end = 0;
     this.html = html;
-    this.seen_start_ends = new Array();
+    this.spanify();
+    colorSpan(this.html, this.html.children[0], next_col);
   }
-  start_state = () => {
-    this.current_char = this.html.innerHTML[0];
-    let [new_txt, new_start, new_end] = replaceCharWithSpan(
-      this.html.innerHTML,
-      0,
-      this.html.innerHTML[0],
-      "text-grey-300 bg-sky-200",
-    );
 
-    this.html.innerHTML = new_txt;
-    this.span_end = new_end;
-    this.span_start = new_start;
-    console.assert(
-      this.html.innerHTML[this.span_end] == ">",
-      "Break in start",
-      new_txt[new_end],
-    );
-  };
   /**
    * update_state
    */
   update_state = (ev: KeyboardEvent) => {
     ev.preventDefault();
+    this.update_forward(ev);
+  };
+  update_forward = (ev: KeyboardEvent) => {
     let color: string;
-    if (ev.key == this.current_char) {
-      color = "text-green-200";
+    let current_char = this.html.children[this.n].innerHTML;
+    if (ev.key == current_char) {
+      color = right_col;
     } else {
-      color = "text-red-500";
+      color = wrong_col;
     }
-    console.assert(
-      this.html.innerHTML[this.span_end] == ">",
-      "Break in start",
-      this.html.innerHTML[this.span_end],
-    );
-
-    let [new_txt, new_start, new_end] = replaceSpanWithSpan(
-      this.html.innerHTML,
-      this.span_start,
-      this.span_end,
-      ev.key,
-      color,
-    );
-    console.assert(
-      new_txt[new_end] == ">",
-      "Break after replace span with span",
-    );
-    [new_txt, new_start, new_end] = replaceCharWithSpan(
-      new_txt,
-      new_end + 1,
-      new_txt[new_end + 1],
-      "text-grey-300 bg-sky-200",
-    );
-    this.html.innerHTML = new_txt;
-    this.span_start = new_start;
-    this.span_end = new_end;
-
-    console.assert(
-      this.html.innerHTML[this.span_end] == ">",
-      "Break after replace char with span",
-    );
+    colorSpan(this.html, this.html.children[this.n], color);
+    this.n++;
+    if (this.n == this.html.children.length) {
+      document.removeEventListener("keydown", this.update_state);
+      return;
+    }
+    colorSpan(this.html, this.html.children[this.n], next_col);
+  };
+  update_backward = (ev: KeyboardEvent) => {
+    if (ev.key != "Backspace") {
+      return;
+    }
+    if (this.n == 0) {
+      return;
+    }
+    colorSpan(this.html, this.html.children[this.n], no_col);
+    this.html.children[this.n];
+    console.log(this.n);
+    this.n--;
+    colorSpan(this.html, this.html.children[this.n], next_col);
+  };
+  spanify = () => {
+    var new_text = new Array();
+    var char: string;
+    for (let i = 0; i < this.html.innerHTML.length; i++) {
+      char = this.html.innerHTML[i];
+      new_text.push(`<span class=${no_col}>` + char + "</span>");
+    }
+    this.html.innerHTML = new_text.join("");
   };
 }
 
-function replaceCharWithSpan(
-  string_: string,
-  index: number,
-  char_to_replace: string,
-  color: string,
-): [string, number, number] {
-  var replacement_string =
-    '<span class = "' + color + '">' + char_to_replace + "</span>";
-  var new_n = index + replacement_string.length - 1;
-  var updated_string =
-    string_.substring(0, index) +
-    replacement_string +
-    string_.substring(index + 1);
-  return [updated_string, index, new_n];
-}
-function replaceSpanWithSpan(
-  string_: string,
-  index_start: number,
-  index_end: number,
-  char_to_replace: string,
-  color: string,
-): [string, number, number] {
-  var replacement_string =
-    '<span class = "' + color + '">' + char_to_replace + "</span>";
-  var new_n = index_start + replacement_string.length - 1;
-  var updated_string =
-    string_.substring(0, index_start) +
-    replacement_string +
-    string_.substring(index_end - 1);
+function colorSpan(parent: Element, span: Element, color: string) {
+  let char = span.innerHTML;
 
-  return [updated_string, index_start, new_n];
+  let new_span = document.createElement("span");
+  new_span.className = color;
+  new_span.innerHTML = char;
+  parent.replaceChild(new_span, span);
 }
 var p = document.getElementById("input_text");
 if (!p) {
@@ -116,6 +76,5 @@ if (!p) {
 console.log("Found input text");
 var n = 0;
 var text_entry = new TextEntry(p);
-text_entry.start_state();
-
 document.addEventListener("keypress", text_entry.update_state);
+document.addEventListener("keydown", text_entry.update_backward);
