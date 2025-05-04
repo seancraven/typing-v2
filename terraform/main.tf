30a8a40586692d8788a8faa9a2834b9c04bc1a61
sean craven <seanpietercraven@gmail.com>
2025-05-04 13:10:28 +0100

    chore readme

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,6 +14,14 @@ terraform {
       version = "4.0.6"
     }
   }
+  cloud {
+
+    organization = "seancraven"
+
+    workspaces {
+      name = "typing"
+    }
+  }
 }
 
 provider "cloudflare" {
@@ -24,15 +32,21 @@ data "cloudflare_zone" "typing_zone" {
   zone_id = "897ed6362c7b1bdd399b8186f2ce0dbe"
 }
 
+resource "tls_private_key" "pem" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+
 resource "tls_cert_request" "csr" {
-  private_key_pem = file("private.key")
+  private_key_pem = tls_private_key.pem.public_key_pem
   subject {
     common_name  = "*.programtype.com"
     organization = " Sean Craven"
   }
 }
 
-resource "cloudflare_origin_ca_certificate" "example_origin_ca_certificate" {
+resource "cloudflare_origin_ca_certificate" "ca_cert" {
   csr                = tls_cert_request.csr.cert_request_pem
   hostnames          = ["programtype.com", "*.programtype.com"]
   request_type       = "origin-rsa"
@@ -40,10 +54,19 @@ resource "cloudflare_origin_ca_certificate" "example_origin_ca_certificate" {
 }
 
 
-resource "cloudflare_dns_record" "dns-record-for-server" {
+resource "cloudflare_dns_record" "www-record-for-server" {
   zone_id = data.cloudflare_zone.typing_zone.zone_id
   type    = "AAAA"
-  name    = "dns"
+  name    = "www"
+  proxied = true
+  content = hcloud_server.web.ipv6_address
+  ttl     = 1
+}
+resource "cloudflare_dns_record" "root-record-for-server" {
+  zone_id = data.cloudflare_zone.typing_zone.zone_id
+  type    = "AAAA"
+  name    = "@"
+  proxied = true
   content = hcloud_server.web.ipv6_address
   ttl     = 1
 }
@@ -54,7 +77,7 @@ provider "hcloud" {
 
 resource "hcloud_ssh_key" "main" {
   name       = "main-ssh"
-  public_key = file("~/.ssh/hcloud_ssh.pub")
+  public_key = file("${path.root}/../.ssh/hcloud_ssh.pub")
 }
 
 resource "hcloud_server" "web" {
@@ -71,4 +94,14 @@ resource "hcloud_server" "web" {
 
 output "hcloud_ip" {
   value = hcloud_server.web.ipv6_address
+}
+
+output "key-pem" {
+  value     = tls_private_key.pem.private_key_pem
+  sensitive = true
+}
+
+output "key-crt" {
+  value     = cloudflare_origin_ca_certificate.ca_cert.certificate
+  sensitive = true
 }
