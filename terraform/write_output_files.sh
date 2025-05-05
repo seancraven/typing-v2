7cb51af719d41825799e3567715a87002206ab0b
sean craven <seanpietercraven@gmail.com>
2025-05-05 16:31:09 +0100

    chore wip

--- terraform/write_output_files.sh
+++ terraform/write_output_files.sh
@@ -6,4 +6,5 @@ terraform output -raw ssh-pri >~/.ssh/ssh
 terraform output -raw ssh-pub >~/.ssh/ssh.pub
 terraform output -raw ssh-pri | gh secret set ssh
 terraform output -raw ssh-pub | gh secret set ssh_pub
+echo -e $GOOGLE_API_KEY | gh secret set google_api_key
 rsync -avP secret type:/mnt/
